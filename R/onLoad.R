6a12890385d29c4312e1f2c4b8a46044d85e8a29
carlosyanez <carlos.yanez.s@gmail.com>
2022-12-21 00:02:35 +1100

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,8 +5,12 @@
   home       <- Sys.getenv("HOME")
   cache_path <- file.path(home, ".auspol_cache")
 
-  if(!dir.exists(cache_path))
+  if(!dir.exists(cache_path)){
     manage_cache_dir(cache_path)
+    Sys.setenv(aussie_cache_dir=cache_path)
+
+  }
+manage_cache_dir(cache_path)
 
 
 }
